a67dee03d7a5fd606bccc40529ad6a8e49f3a4e3
Arthur Burkart <artburkart@gmail.com>
2015-07-23 15:08:12 -0400

    Add docker commands

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from flask import abort
 from flask import Flask
 from flask import jsonify
 from flask import render_template
 from flask import request
+from subprocess import check_output
+
 app = Flask(__name__)
 
 
@@ -12,7 +15,18 @@ def index():
 
 @app.route('/api', methods=['POST'])
 def api():
+    req = request.get_json()
+    branch = req.get('branch', 'red')
+    repo = req.get('repo', 'colored_pages')
+    subdomain = req.get('subdomain')
+    if subdomain is None:
+        abort(403)
+    cmd = 'docker kill {0} && docker rm {0}'.format(branch)
+    check_output(cmd.strip().split())
+    cmd = 'docker run -e "BRANCH={}" -e "VIRTUAL_HOST={}.chaxster.com" {}'.format(branch, subdomain, repo)
+    check_output(cmd.strip().split())
     return jsonify(request.get_json())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
