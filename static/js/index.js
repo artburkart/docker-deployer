ea5c1d475ab84438002e8ea6fcdfac561fea373c
CoryNoonan <cory@maxwellhealth.com>
2015-07-23 15:08:54 -0400

    script tags using url syntax for flask external JS achieved

---
+++ static/js/index.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+  $.fn.serializeObject = function()
+  {
+      var o = {};
+      var a = this.serializeArray();
+      $.each(a, function() {
+          if (o[this.name] !== undefined) {
+              if (!o[this.name].push) {
+                  o[this.name] = [o[this.name]];
+              }
+              o[this.name].push(this.value || '');
+          } else {
+              o[this.name] = this.value || '';
+          }
+      });
+      return o;
+  };
+  $("#deploy-form").on("submit",function(e) {
+    e.preventDefault();
+    var url = "/api";
+    var form = $("#deploy-form").serializeObject();
+      form.repo = 'github.com/artburkart/colored_pages';
+    console.log(form);
+    $.ajax({
+      url: url, 
+      type: 'post',
+      data: JSON.stringify(form),
+      contentType: "application/json",
+      datatype : "json",
+      success: function(data) {
+        alert("You've Deployed!");
+      }
+   });
+  });
+}); 
